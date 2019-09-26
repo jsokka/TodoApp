55c28200847bcaa78dc5a79b32da2a462466f1b6
Jarmo Sokka <jsokka@hotmail.com>
2019-09-26 19:59:18 +0300

    EF Core fixes. Migration Fixes

--- src/TodoApp.Data/Models/BaseEntity.cs
+++ src/TodoApp.Data/Models/BaseEntity.cs
@@ -6,7 +6,7 @@ using System.Text;
 
 namespace TodoApp.Data.Models
 {
-    [NotMapped]
+    //[NotMapped]
     public abstract class BaseEntity
     {
         [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
