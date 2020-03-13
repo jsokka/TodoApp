de865a518741986735ae7432997a9c018af19b2c
Jarmo Sokka <jsokka@hotmail.com>
2020-03-13 20:22:38 +0200

    Update client packages

--- src/TodoApp.Client/src/App.js
+++ src/TodoApp.Client/src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { Route, Redirect, Switch } from 'react-router';
 import Layout from './Layout';
 import Tasks from './components/Tasks/Tasks';
-import "./App.scss";
 
 export default class App extends Component {
   displayName = App.name
