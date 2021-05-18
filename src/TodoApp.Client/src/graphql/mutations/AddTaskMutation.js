e5163a5ce80c9fb1a520935565c166e12a7bd469
Jarmo Sokka <jsokka@hotmail.com>
2021-05-18 20:45:17 +0300

    Disable import/no-anonymous-default-export warnings

--- src/TodoApp.Client/src/graphql/mutations/AddTaskMutation.js
+++ src/TodoApp.Client/src/graphql/mutations/AddTaskMutation.js
@@ -14,6 +14,7 @@ const mutation = graphql`
   }
 `
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default (title, projectId, callback) => {
   const variables = {
     taskInput: {
--- src/TodoApp.Client/src/graphql/mutations/UpdateTaskMutation.js
+++ src/TodoApp.Client/src/graphql/mutations/UpdateTaskMutation.js
@@ -11,6 +11,7 @@ const mutation = graphql`
   }
 `
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default (task, callback) => {
   const variables = {
     taskId: task.id,
